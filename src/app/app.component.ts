b4b406ca89089652b8a75943fa3329f8d05432af
lythx <lythxxx@gmail.com>
2022-12-10 23:23:25 +0100

    date and input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'atari_archive';
+export class AppComponent implements OnInit {
+
+  readonly title = 'atari_archive';
+  date: number | undefined
+
+  ngOnInit() {
+    setInterval(() => {
+      this.date = Date.now()
+    }, 300)
+  }
+
 }
