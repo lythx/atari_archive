1ff8b2fa6f2e252fe8c990334b261c0fe6e1f53a
lythx <lythxxx@gmail.com>
2022-12-11 23:46:52 +0100

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   readonly title = 'atari_archive';
-  date: number | undefined
+  date!: number
+  input!: string
+  displayGrid = false
+  czasopismaXml!: Document
+  readonly validationRegExp = /^[0-9]*$/
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
     setInterval(() => {
       this.date = Date.now()
     }, 300)
+    const params = new HttpParams(
+
+    )
+    this.httpClient.get("./assets/czasopisma.xml", {
+      params,
+      responseType: 'text'
+    }).subscribe((data) => {
+      const parser = new DOMParser();
+      this.czasopismaXml = parser.parseFromString(data, "text/xml");
+    })
+  }
+
+  handleInput() {
+    // TODO VALIDATE
+
+    if (this.input === '666.666') {
+      this.displayGrid = true
+    }
   }
 
 }
